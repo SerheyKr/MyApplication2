3dfef849c4f4634c8c66617e6363a66971083995
Serhey <46533559+SergeyInfantry@users.noreply.github.com>
2023-03-13 23:33:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/AnimalData.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import android.graphics.Bitmap
+
+data class AnimalData (
+    var name: String,
+    var descriptionShort: String,
+    var imageId: Int,
+    var descriptionLong: String
+)
