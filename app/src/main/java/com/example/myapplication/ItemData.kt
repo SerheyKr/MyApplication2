0a9fd3e0d3e87567b20375742b5f4ab5f8d17cc7
Serhey <sergey43433434@gmail.com>
2023-04-07 18:45:46 +0300

    Competed second lab

--- app/src/main/java/com/example/myapplication/ItemData.kt
+++ app/src/main/java/com/example/myapplication/ItemData.kt
@@ -1,12 +1,9 @@
 package com.example.myapplication
 
-import android.graphics.Bitmap
-import android.graphics.Color
-
-data class AnimalData (
+data class ItemData
+    (
     var name: String,
     var descriptionShort: String,
     var imageId: Int,
-    var textColor: Int,
     var descriptionLong: String
 )
